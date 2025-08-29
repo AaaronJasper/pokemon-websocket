7c116cecd160c62a4a2dc6d85947628a70be98e1
davissssss <davis23232323@gmail.com>
2025-08-29 15:41:19 +0800

    add: add comment

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const io = new Server(httpServer, {
 });
 
 // For React, connect websocket for fronend
+// Use httpServer for socket.io to turn http to websocket
 io.on("connection", (socket) => {
   console.log("Client connected", socket.id);
 
@@ -28,12 +29,15 @@ io.on("connection", (socket) => {
 });
 
 // For Laravel, receive request from backend
+// Use Express server only for http API (Express is framework is easier to use)
 app.post("/broadcast", (req, res) => {
   const { channel, event, data } = req.body;
   io.to(channel).emit(event, data);
   console.log(`broadcast ${event} to channel ${channel}`, data);
   res.sendStatus(200);
 });
+
+//----------------------------------------------------------------------------------
 
 app.listen(6002, () => {
   console.log("HTTP server (for Laravel) running on port 6002");
